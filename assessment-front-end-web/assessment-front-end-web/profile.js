14d97684db54e7c16deb21e9020280e5b81dbf96
Morgan Herrmann <morganherrmann4@gmail.com>
2023-03-20 09:08:42 -0700

    updated assessment

--- assessment-front-end-web/assessment-front-end-web/profile.js
+++ assessment-front-end-web/assessment-front-end-web/profile.js
@@ -1,6 +1,8 @@
 console.log('JS Connected!')
 
-let favColor = document.querySelector('#color')
+
+
+let favColor = document.querySelector("#color")
 
 const favoriteColor = () => {
     alert('Green is the best color ever!')
@@ -10,7 +12,7 @@ favColor.addEventListener('click', favoriteColor)
 
 
 
-let favPlace = document.querySelector('#place')
+let favPlace = document.querySelector("#place")
 
 const favoritePlace = () => {
     alert('My favorite would probably have to be my bed')
@@ -20,7 +22,7 @@ favPlace.addEventListener('click', favoritePlace)
 
 
 
-let favRitual = document.querySelector('#ritual')
+let favRitual = document.querySelector("#ritual")
 
 const favoriteRitual = () => {
     alert('My favorite ritual is listening to my favorite songs on a loop 24/7')
